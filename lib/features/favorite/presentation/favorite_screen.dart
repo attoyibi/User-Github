e1e1da9caa3e8c181996f1db92929ce6f406cbf6
muhammadmuchson <muhammadmuchson@gmail.com>
2024-12-04 07:54:55 +0000

    favorot screen

--- lib/features/favorite/presentation/favorite_screen.dart
+++ lib/features/favorite/presentation/favorite_screen.dart
@@ -5,12 +5,47 @@ class FavoriteScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: const Text('Github Apps Demo'),
+        bottom: const TabBar(
+          tabs: [
+            Tab(icon: Icon(Icons.star), text: 'Popular'),
+            Tab(icon: Icon(Icons.favorite), text: 'Favorite'),
+          ],
+        ),
       ),
       body: Center(
-        child: Text(
-          'Hello World Favotir',
-          style: TextStyle(fontSize: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.thumb_up,
+                size: 100,
+                color: Colors
+                    .green), // Replace with actual clapping hands icon if available
+            const SizedBox(height: 20),
+            const Text(
+              'Yeay, Data Favorit Kosong',
+              style: TextStyle(fontSize: 18, color: Colors.green),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Silahkan Refresh Page untuk memperbarui data',
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Implement refresh logic here
+              },
+              child: const Text('Refresh'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.lightBlueAccent,
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
